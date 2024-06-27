9249b15e9c4a84a2720319fbd6748200955b7c1c
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2024-06-27 16:27:17 -0300

    feat: first route with prefix

--- router/router.go
+++ router/router.go
@@ -3,11 +3,9 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	// initialize router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run(":8080") // listen and serve on 0.0.0.0:8080
+	InitializeRoutes(router)
+	// listen and serve on 0.0.0.0:8080
+	router.Run(":8080")
 }
