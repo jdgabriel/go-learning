f2f5da0104ba9057581a36698e80bad392836143
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2024-06-28 16:02:13 -0300

    feat: add get opening handler

--- handler/get-opening-handler.go
+++ handler/get-opening-handler.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jdgabriel/go-learning/schema"
 )
 
 func GetOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Hello World 2",
-	})
+	openings := []schema.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
 }
